465bfbf554e5587a0dffa30dd60d1d4a1ea01087
Mauro <mowa_mauro95@hotmail.com>
2020-05-09 18:54:45 -0300

    Cola de mensajes de new pokemon recibiendo mensajes

--- broker/broker.c
+++ broker/broker.c
@@ -1,19 +1,144 @@
 #include <conexiones.h>
-#
+#include "broker.h"
 
 int main(int argc, char *argv[]){
 
 	char * configPath = "../broker.config";
 	char * ipconfig= "IP_BROKER";
 	char * puertocofing= "PUERTO_BROKER";
+	t_list* suscriptores = malloc(sizeof(t_list));
+	t_list* mensajes = malloc(sizeof(t_list));
+	new_pokemon = malloc(sizeof(t_cola_mensaje));
+	(*new_pokemon).suscriptores = suscriptores;
+	(*new_pokemon).mensajes = mensajes;
 
-	t_log *logger;
+	cantidad_mensajes = 0;
 
-	logger =log_create("../broker.log", "BROKER", false, LOG_LEVEL_INFO);
-	log_info(logger, "ESTOY LOGEANDO");
+	logger_broker =log_create("../broker.log", "BROKER", false, LOG_LEVEL_INFO);
+	logger_global = log_create("../global.log", "GLOBAL", false, LOG_LEVEL_INFO);
+	log_info(logger_broker, "ESTOY LOGEANDO");
 
-	iniciar_servidor(configPath, ipconfig,puertocofing);
+	iniciar_servidor(configPath, ipconfig, puertocofing);
 
 	return EXIT_SUCCESS;
 
 }
+
+void iniciar_servidor(char* path_config, char* ip_config, char* port_config)
+{
+	int socket_servidor;
+	char* ip;
+	char* puerto;
+    struct addrinfo hints, *servinfo, *p;
+	t_config* config;
+
+
+	config = config_create(path_config);
+
+	ip = config_get_string_value(config, ip_config);
+	puerto = config_get_string_value(config, port_config);
+
+    memset(&hints, 0, sizeof(hints));
+    hints.ai_family = AF_UNSPEC;
+    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_flags = AI_PASSIVE;
+
+    getaddrinfo(ip, puerto, &hints, &servinfo);
+
+    for (p=servinfo; p != NULL; p = p->ai_next)
+    {
+        if ((socket_servidor = socket(p->ai_family, p->ai_socktype, p->ai_protocol)) == -1)
+            continue;
+
+        if (bind(socket_servidor, p->ai_addr, p->ai_addrlen) == -1) {
+            close(socket_servidor);
+            continue;
+        }
+        break;
+    }
+
+	listen(socket_servidor, SOMAXCONN);
+
+    freeaddrinfo(servinfo);
+
+	//config_destroy(config);
+
+    while(1)
+    	esperar_cliente(socket_servidor);
+}
+
+void esperar_cliente(int socket_servidor)
+{
+	struct sockaddr_in dir_cliente;
+
+	socklen_t tam_direccion = sizeof(struct sockaddr_in);
+
+	int socket_cliente = accept(socket_servidor, (void*) &dir_cliente, &tam_direccion);
+
+	pthread_create(&thread,NULL,(void*)serve_client,&socket_cliente);
+	pthread_detach(thread);
+
+}
+
+void serve_client(int* socket)
+{
+	op_code cod_op;
+	if(recv(*socket, &cod_op, sizeof(op_code), MSG_WAITALL) == -1)
+		cod_op = -1;
+	process_request(cod_op, *socket);
+}
+
+void process_request(int cod_op, int socket) {
+	uint32_t size;
+	void* msg;
+	puntero_mensaje_new_pokemon mensajeRecibido;
+	t_mensaje* mensaje = malloc(sizeof(t_mensaje));
+		switch (cod_op) {
+		case MESSAGE:
+			msg = server_recibir_mensaje(socket, &size);
+			msg = "1";
+			size = sizeof(4);
+			devolver_mensaje(msg, size, socket);
+			log_info(logger_broker, "MESSAGE");
+			log_info(logger_broker, msg);
+			free(msg);
+			break;
+		case NEW_POKEMON:
+			log_info(logger_broker, "ENTRA");
+
+			mensajeRecibido = recibir_new_pokemon(socket, &size, logger_broker);
+			log_info(logger_broker, "SALE");
+
+			mensaje->id = cantidad_mensajes;
+			mensaje->mensaje = mensajeRecibido;
+
+			list_add(new_pokemon->mensajes, mensaje);
+
+			char* id_mensaje = string_itoa(cantidad_mensajes);
+			devolver_mensaje(id_mensaje, strlen(id_mensaje) + 1, socket);
+
+			aumentar_cantidad_mensajes();
+
+			log_info(logger_broker, "NEWPOKEMON");
+
+			free(mensajeRecibido);
+			free(mensaje);
+			break;
+		case 0:
+			pthread_exit(NULL);
+		case -1:
+			pthread_exit(NULL);
+		}
+}
+
+void aumentar_cantidad_mensajes(){
+	cantidad_mensajes ++;
+}
+
+/*void* server_recibir_mensaje(int socket_cliente, uint32_t* size)
+{
+	void * buffer = malloc(*size);
+	recv(socket_cliente, buffer, *size, MSG_WAITALL);
+
+	return buffer;
+}*/
--- conexiones/conexiones.h
+++ conexiones/conexiones.h
@@ -19,8 +19,6 @@
 #include<string.h>
 #include<pthread.h>
 
-
-
 typedef enum
 {
 	MESSAGE=1,
@@ -48,15 +46,63 @@ typedef struct
 
 } t_package;
 
+typedef struct {
+	int id;
+	void* mensaje;
+	int id_correlacional;
+	t_list* suscriptores_enviados;
+	t_list* suscriptores_ack;
+} t_mensaje;
+
+typedef struct {
+	uint32_t name_size;
+	char* name_pokemon;
+	uint32_t pos_x;
+	uint32_t pos_y;
+	uint32_t quant_pokemon;
+} t_mensaje_new_pokemon;
+
+typedef t_mensaje_new_pokemon* puntero_mensaje_new_pokemon;
+
+typedef struct {
+	uint32_t name_size;
+	char* name_pokemon;
+	uint32_t quant_pokemon;
+	t_list* coords; // TODO lista de pares de coordenadas
+} t_mensaje_localized_pokemon;
+
+typedef struct {
+	uint32_t name_size;
+	char* name_pokemon;
+} t_mensaje_get_pokemon;
+
+typedef struct {
+	uint32_t name_size;
+	char* name_pokemon;
+	uint32_t pos_x;
+	uint32_t pos_y;
+} t_mensaje_appeared_pokemon;
+
+typedef struct {
+	uint32_t name_size;
+	char* name_pokemon;
+	uint32_t pos_x;
+	uint32_t pos_y;
+} t_mensaje_catch_pokemon;
+
+typedef struct {
+	uint32_t caught_pokemon;
+} t_mensaje_caught_pokemon;
+
 
 pthread_t thread;
-
+t_log* logger_global;
 // Server
 
 void* recibir_buffer(int*, int);
 void iniciar_servidor(char* path_config, char* ip_config, char* port_config);
 void esperar_cliente(int);
-void* server_recibir_mensaje(int socket_cliente, int* size);
+void* server_recibir_mensaje(int socket_cliente, uint32_t* size);
 int recibir_operacion(int);
 void process_request(int cod_op, int cliente_fd);
 void serve_client(int *socket);
@@ -70,6 +116,6 @@ void enviar_mensaje(char* mensaje, int socket_cliente);
 char* client_recibir_mensaje(int socket_cliente);
 void eliminar_paquete(t_package* paquete);
 void liberar_conexion(int socket_cliente);
-
+void* serializar_paquete(t_package* paquete, int* bytes);
 
 #endif /* CONEXIONES_H_ */
